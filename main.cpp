24ee7ca27ca5e010554a3a144d6e9139e4998146
Abdur Rafay <99770288+AbdurRafay2004@users.noreply.github.com>
2024-03-27 20:57:05 +0000

    general draft

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+/*
+Project info
+Employee Payroll System:
+ - Design classes for employees, departments, and payrolls, HR.
+ - Implement functionalities like calculating salaries, adding new employees, and displaying payroll information.
+*/
 #include <iostream>
+using namespace std;
+
+class HR{
+
+};
+class departments{
+
+};
+class employees{
+
+};
+class payroll{
+
+};
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  std::cout << "Hello there!\n";
+
 }
